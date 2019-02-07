6433600d584375f6db646911eaeabed2a8f2b3f7
ttcom <ttcom>
2019-02-07 13:35:35 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,103 @@
+<template>
+  <div id="app">
+   <h3>树形控件开始拖拽事件，没有回调参数产生</h3>
+          
+ <a-button type="primary">Button</a-button>
+  <a-tree
+    @expand="onExpand"
+    :expandedKeys="expandedKeys"
+    :autoExpandParent="autoExpandParent"
+    v-model="checkedKeys"
+    @select="onSelect"
+    :selectedKeys="selectedKeys"
+    :treeData="treeData"
+    draggable
+    @dragStart="OnDragStart"
+  />
+  </div>
+</template>
+<script>
+const treeData = [{
+  title: '0-0',
+  key: '0-0',
+  children: [{
+    title: '0-0-0',
+    key: '0-0-0',
+    children: [
+      { title: '0-0-0-0', key: '0-0-0-0' },
+      { title: '0-0-0-1', key: '0-0-0-1' },
+      { title: '0-0-0-2', key: '0-0-0-2' },
+    ],
+  }, {
+    title: '0-0-1',
+    key: '0-0-1',
+    children: [
+      { title: '0-0-1-0', key: '0-0-1-0' },
+      { title: '0-0-1-1', key: '0-0-1-1' },
+      { title: '0-0-1-2', key: '0-0-1-2' },
+    ],
+  }, {
+    title: '0-0-2',
+    key: '0-0-2',
+  }],
+}, {
+  title: '0-1',
+  key: '0-1',
+  children: [
+    { title: '0-1-0-0', key: '0-1-0-0' },
+    { title: '0-1-0-1', key: '0-1-0-1' },
+    { title: '0-1-0-2', key: '0-1-0-2' },
+  ],
+}, {
+  title: '0-2',
+  key: '0-2',
+}]
+
+export default {
+  data () {
+    return {
+      expandedKeys: ['0-0-0', '0-0-1'],
+      autoExpandParent: true,
+      checkedKeys: ['0-0-0'],
+      selectedKeys: [],
+      treeData,
+    }
+  },
+  watch: {
+    checkedKeys(val) {
+      console.log('onCheck', val)
+    }
+  },
+  methods: {
+  	
+  	OnDragStart(value){
+  		console.log("开始拖拽了",value)
+  	},
+    onExpand (expandedKeys) {
+      console.log('onExpand', expandedKeys)
+      // if not set autoExpandParent to false, if children expanded, parent can not collapse.
+      // or, you can remove all expanded children keys.
+      this.expandedKeys = expandedKeys
+      this.autoExpandParent = false
+    },
+    onCheck (checkedKeys) {
+      console.log('onCheck', checkedKeys)
+      this.checkedKeys = checkedKeys
+    },
+    onSelect (selectedKeys, info) {
+      console.log('onSelect', info)
+      this.selectedKeys = selectedKeys
+    },
+  },
+}
+
+</script>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
